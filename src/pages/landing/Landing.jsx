b4a5b5655883ba6a641ee2b83f7b615ab5727187
MahdiAghaali <mahdi.aghaali@yahoo.com>
2023-03-28 10:42:38 +0430

    Add starting components

---
+++ src/pages/landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProfilePictureCard from '../../components/ProfilePictureCard/ProfilePictureCard';
+import './landing.scss';
+
+const Landing = () => (
+  <section>
+    <ProfilePictureCard />
+    <h1> Hi, I&apos;m Mahdi Aghaali</h1>
+  </section>
+);
+
+export default Landing;
