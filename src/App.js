54ff90362f18f33749981527b08abae6566df71f
MahdiAghaali <mahdi.aghaali@yahoo.com>
2023-04-04 13:51:57 +0430

    Add route fortesting the feature

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import ToolSpinner from './components/ToolSpinner/ToolSpinner';
 import Landing from './pages/landing/Landing';
 import Projects from './pages/projects/Projects';
 import Social from './pages/Social/Social';
 
 const App = () => (
-  <>
-    <Header />
-    <Landing />
-    <Projects />
-    <Social />
-  </>
+
+  <Routes>
+    <Route
+      path="/"
+      element={(
+        <>
+          <Header />
+          <Landing />
+          <Projects />
+          <Social />
+        </>
+      )}
+    />
+    <Route path="/test" element={<ToolSpinner />} />
+  </Routes>
 );
 
 export default App;
